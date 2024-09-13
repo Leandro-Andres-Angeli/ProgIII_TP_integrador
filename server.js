83b0d989f75b78b50965711fc4313b1fcf65f486
= <=>
2024-09-13 16:11:52 -0300

    get claims by user done

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.SERVER_PORT || 3001;
 
 const server = express();
 server.use(express.json());
-
+//POST  CLAIM
 server.post('/api/claim', async (req, res) => {
   try {
     const { asunto, descripcion, idReclamoTipo } = req.body;
@@ -17,7 +17,7 @@ server.post('/api/claim', async (req, res) => {
       'INSERT INTO reclamos (asunto , descripcion , fechaCreado,idReclamoTipo , idReclamoEstado , idUsuarioCreador) VALUES (?,?,?,?,1,1)',
       [asunto, descripcion, new Date(), idReclamoTipo]
     );
-    console.log(newClaimQuery);
+
     if (newClaimQuery.affectedRows === 0) {
       return res
         .status(500)
@@ -31,9 +31,33 @@ server.post('/api/claim', async (req, res) => {
     res.status(500).json({ ok: false, message: 'Error de servidor' });
   }
 });
+//GET  USER CLAIMS
+
+server.get('/api/claims/:clientId', async (req, res) => {
+  const clientId = Number(req.params.clientId);
+  console.log(clientId);
+
+  const connection = await pool.getConnection();
+  const [getClaimsByClientId] = await connection.query(
+    'SELECT * FROM `reclamos` r  where r.idUsuarioCreador=? ',
+    [clientId]
+  );
+  if (getClaimsByClientId.length === 0) {
+    return res
+      .status(404)
+      .json({ ok: true, message: 'No hay reclamos para este usuario' });
+  }
+  console.log(getClaimsByClientId);
+  connection.release();
+  return res.status(200).json({ ok: true, claims: getClaimsByClientId });
+});
+
+//GET  USER CLAIMS
+
 server.get('/*', (req, res) => {
   return res.status(404).json({ message: 'no existe ruta' });
 });
 server.listen(PORT, () => {
   console.log(`running on ${PORT}`);
 });
+//POST  CLAIM
