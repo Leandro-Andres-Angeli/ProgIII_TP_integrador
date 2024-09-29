e62a3570e44df833d8594daa0c3e25b8a46a4e24
Nahir-Orellana-Lopez <nahir.orellana.lopez@gmail.com>
2024-09-29 16:59:25 -0300

    Se actualizó validación de autorización

--- src/middleware/authorization.js
+++ src/middleware/authorization.js
@@ -2,7 +2,7 @@ const service = require('../services/usuarioService');
 
 const isAdmin = async (req, res, next) => {
   const usuario = await service.getUsuarioById(req.body.user.idUsuario);
-  if (usuario.descripcion != 'admin') {
+  if (usuario.descripcion != 'Administrador') {
     return res.status(403).send({
       ok: false,
       error: 'No está autorizado.',
@@ -13,7 +13,7 @@ const isAdmin = async (req, res, next) => {
 
 const isClient = async (req, res, next) => {
   const usuario = await service.getUsuarioById(req.body.user.idUsuario);
-  if (usuario.descripcion != 'cliente') {
+  if (usuario.descripcion != 'Cliente') {
     return res.status(403).send({
       ok: false,
       error: 'No está autorizado.',
