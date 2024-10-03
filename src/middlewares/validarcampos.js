1e5215359c8da754a40fa0a9509ce79fcefc06cf
Walter Jesus Aguirre <aguirrewalterjesus@gmail.com>
2024-10-02 22:55:24 -0300

    express funcionando

--- src/middlewares/validarcampos.js
+++ src/middlewares/validarcampos.js
@@ -1,7 +1,7 @@
 const {validationResult} = require('express-validator');
 
 const validarCampos = (req,res,next) => {
-    const errors = ValidationResult (req);
+    const errors = validationResult (req);
     if (!errors.isEmpty()){
         return res.status (400).json({ok: false, errors: errors.mapped()});
     }
